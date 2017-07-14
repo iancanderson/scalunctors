1f466b6454c4978ea45eee6ec88297685c5d514c
Ian C. Anderson <ian@iancanderson.com>
2017-07-14 13:07:11 -0400

    Load rhymes from RhymeZone API

---
+++ Puns.scala
@@ -0,0 +1,31 @@
+package com.thoughtbot
+
+import play.api.libs.json._
+
+case class Rhyme(word: String, score: Int)
+
+object Puns {
+  implicit val rhymeReads = Json.reads[Rhyme]
+
+  def get(url: String) = scala.io.Source.fromURL(url).mkString
+
+  def getJson(url: String): JsValue = {
+    val body = get(url)
+    Json.parse(body)
+  }
+
+  def getRhymes(word: String): List[Rhyme] = {
+    val url = f"http://rhymebrain.com/talk?function=getRhymes&word=$word"
+    val result = getJson(url)
+    val rhymesResult: JsResult[List[Rhyme]] = Json.fromJson[List[Rhyme]](result)
+
+    rhymesResult match {
+      case JsSuccess(rhymes: List[Rhyme], path: JsPath) => rhymes
+      case e : JsError => List()
+    }
+  }
+
+  def main(args: Array[String]) {
+    println(getRhymes("beer"))
+  }
+}
